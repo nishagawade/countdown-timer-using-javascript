167c2d578cc92e296e26a60fc898afa5044e6d0b
nisha.g <nisha.g@finacus.co.in>
2024-06-24 15:41:52 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,40 @@
+function countdownTimer(endDate) {
+    let days, hours, minutes, seconds;
+
+    endDate = new Date(endDate).getTime();
+
+    if (isNaN(endDate)) {
+        return;
+    }
+
+    setInterval(calculate, 1000);
+
+    function calculate() {
+        let startDate = new Date().getTime();
+
+        let timeRemaining = parseInt((endDate - startDate) / 1000);
+
+        if (timeRemaining >= 0) {
+            days = parseInt(timeRemaining / 86400);
+            timeRemaining = (timeRemaining % 86400);
+
+            hours = parseInt(timeRemaining / 3600);
+            timeRemaining = (timeRemaining % 3600);
+
+            minutes = parseInt(timeRemaining / 60);
+            seconds = parseInt(timeRemaining % 60);
+
+            document.getElementById("days").innerHTML = parseInt(days, 10);
+            document.getElementById("hours").innerHTML = ("0" + hours).slice(-2);
+            document.getElementById("minutes").innerHTML = ("0" + minutes).slice(-2);
+            document.getElementById("seconds").innerHTML = ("0" + seconds).slice(-2);
+        } else {
+            return;
+        }
+    }
+}
+
+(function () {
+    const endDate = "June 30, 2024 00:00:00";
+    countdownTimer(endDate);
+}());
